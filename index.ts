d35b1c7e27f248730a280ed5bcf5ae043bc966ed
priosshrsth <shrsthprios@hotmail.com>
2020-06-06 16:34:31 +0545

    initial commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { FbHook, HookOptions } from './src/fbHook';
+export { Conversation } from './src/Conversation';
+export { Chat, ChatOptions } from './src/Chat';
---
+++ providers/FbHookProvider.ts
@@ -0,0 +1,34 @@
+// @ts-ignore
+import { IocContract } from '@adonisjs/fold'
+import {FbHook, HookOptions} from '../index';
+
+
+export default class FbHookProvider {
+  constructor (protected $container: IocContract) {
+  }
+  
+  public register () {
+    // Register your own bindings
+    this.$container.singleton('Adonis/Addons/FbHook', (app) => {
+      const env = app.use('Adonis/Core/Env')
+      return new FbHook(<HookOptions> {
+        accessToken: env.get('FB_ACCESS_TOKEN'),
+        appSecret: env('FB_APP_SECRET'),
+        verifyToken: env("FB_VERIFY_TOKEN"),
+        allowTypingIndicator: true,
+      })
+    })
+  }
+  
+  public boot () {
+    // IoC container is ready
+  }
+  
+  public shutdown () {
+    // Cleanup, since app is going down
+  }
+  
+  public ready () {
+    // App is ready
+  }
+}
